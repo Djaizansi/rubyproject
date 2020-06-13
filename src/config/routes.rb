247c3b5201668811ee2dc7f05071c522074bf4be
Djaizansi <youcef.jallali@gmail.com>
2020-06-13 23:47:27 +0200

    Mise en place des routes pour Roles, pour les pages 404, 500 et 422

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
 
+  resources :roles
   root controller: :home, action: :home
   devise_for :users
   resources :categories
   resources :comments
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :posts # localhost:3000/posts
+
+
+  get '/404', to: 'erreurs#page_introuvable'
+  get '/422', to: 'erreurs#unacceptable'
+  get '/500', to: 'erreurs#erreur_interne'
+
 end
