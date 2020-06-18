9bbb229737f934bee9cd492b4a960db9dc97cd55
Djaizansi <youcef.jallali@gmail.com>
2020-06-18 11:34:19 +0200

    Ajout de donnée à charger

--- src/db/seeds.rb
+++ src/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Role.create(:name => "admin")
+Role.create(:name => "user")
+
+user = User.create(username: 'admin', email:'admin@admin.fr', password: 'azerty')
+user.roles << Role.find_by(name: 'admin')
