c1848a57d2b46a127254c3418fa4f1fbef99b469
Djaizansi <youcef.jallali@gmail.com>
2020-06-19 17:56:00 +0200

    Ajout des differentes reponses selon le format demandé

--- src/app/controllers/application_controller.rb
+++ src/app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+    respond_to :html, :json
     protect_from_forgery with: :exception
     before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -12,4 +13,5 @@ class ApplicationController < ActionController::Base
     rescue_from CanCan::AccessDenied do |exception|
         redirect_to root_path, alert: "Accès interdit: Vous n'avez pas les droits"
     end
+
 end
