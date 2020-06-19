e359cb74e380d9723c0a184b1925c6e64dd26222
Djaizansi <youcef.jallali@gmail.com>
2020-06-19 18:52:01 +0200

    Ajout des confirmations

--- src/app/models/user.rb
+++ src/app/models/user.rb
@@ -4,13 +4,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-        :recoverable, :rememberable, :validatable
+        :recoverable, :rememberable, :validatable, :confirmable
 
   validates :username, presence: true
 
   after_create :assign_default_role
-
-  has_and_belongs_to_many :roles, :join_table => :users_roles
 
   def assign_default_role
     self.add_role(:user) if self.roles.blank?
