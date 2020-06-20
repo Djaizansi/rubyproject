366a5c843a95c34488da3e2435ab7c0af5e634bf
Marwane <manbou92@hotmail.fr>
2020-06-20 15:48:48 +0200

    Création de la route Media

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :posts # localhost:3000/posts
 
+  resources :media
+
   default_url_options :host => "localhost:3000"
 
 
